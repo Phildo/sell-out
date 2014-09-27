0ad6f989782335ee3670f92e9d9ac91eef215bd4
Phildo <phildo211@gmail.com>
2014-09-27 18:58:17 -0500

    initial commit (based on js-mini-start)

---
+++ src/scenes/gameplay/gameplayScene.js
@@ -0,0 +1,22 @@
+var GamePlayScene = function(game, canv)
+{
+  var self = this;
+
+  self.ready = function()
+  {
+  };
+
+  self.tick = function()
+  {
+  };
+
+  self.draw = function()
+  {
+  };
+
+  self.cleanup = function()
+  {
+  };
+
+};
+
---
+++ src/scenes/loading/loadingScene.js
@@ -0,0 +1,55 @@
+var LoadingScene = function(game, canv)
+{
+  var pad;
+  var barw;
+  var progress;
+
+  var imagesloaded = 0;
+  var images = [];
+
+  var imageLoaded = function()
+  {
+    imagesloaded++;
+  };
+
+  this.ready = function()
+  {
+    pad = 20;
+    barw = (canv.canvas.width-(2*pad));
+    progress = 0;
+    //canv.context.fillStyle = "#000000";
+    //canv.context.font = "20px vg_font";
+    //canv.context.fillText(".",0,0);// funky way to encourage the custom font to load
+
+    for(var i = 0; i < images.length; i++)
+    {
+      images[i] = new Image();
+      images[i].onload = imageLoaded; 
+    }
+    /*
+    images[0].src = "assets/man.png";
+    */
+    imageLoaded(); //call once to prevent 0/0 != 100% bug
+  };
+
+  this.tick = function()
+  {
+    if(progress <= imagesloaded/(images.length+1)) progress += 0.01;
+    if(progress >= 1.0) game.nextScene();
+  };
+
+  this.draw = function()
+  {
+    canv.context.fillRect(pad,canv.canvas.height/2,progress*barw,1);
+    canv.context.strokeRect(pad-1,(canv.canvas.height/2)-1,barw+2,3);
+  };
+
+  this.cleanup = function()
+  {
+    progress = 0;
+    imagesloaded = 0;
+    images = [];//just used them to cache assets in browser; let garbage collector handle 'em.
+    canv.context.fillStyle = "#FFFFFF";
+    canv.context.fillRect(0,0,canv.canvas.width,canv.canvas.height);
+  };
+};
